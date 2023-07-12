8e8f9296338c2c11f8d4475971942b72a898ca02
USER <USER@DESKTOP-A136ETG>
2023-07-12 06:46:01 +0530

    Multiple Checkbox and Multiple RadioButton check added

--- src/test/java/com/obsqura/TestNGFramework/CheckBoxDemo.java
+++ src/test/java/com/obsqura/TestNGFramework/CheckBoxDemo.java
@@ -1,5 +1,8 @@
 package com.obsqura.TestNGFramework;
 
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -14,6 +17,57 @@ public class CheckBoxDemo extends Base {
 		checkBoxClick.click();
 		Boolean isCheckBoxIsSelected = checkBoxClick.isSelected();
 		
+	}
+	
+	
+	@Test
+	public void checkMultipleCheckBoxSelected()
+	{
+		driver.navigate().to("https://selenium.obsqurazone.com/check-box-demo.php");
+		WebElement selectAllButton = driver.findElement(By.xpath("//input[@id='button-two']"));
+		selectAllButton.click();
+		boolean isSelectAllButtonDisplayed = selectAllButton.isDisplayed();
+
+	    WebElement checkBoxOne = driver.findElement(By.xpath("//input[@id='check-box-one']"));
+		boolean isCheckBoxOneSelected = checkBoxOne.isSelected();
+		WebElement checkBoxTwo = driver.findElement(By.xpath("//input[@id='check-box-two']"));
+		boolean isCheckBoxTwoSelected = checkBoxTwo.isSelected();
+		WebElement checkBoxThree = driver.findElement(By.xpath("//input[@id='check-box-three']"));
+		boolean isCheckBoxThreeSelected = checkBoxThree.isSelected();
+		WebElement checkBoxFour = driver.findElement(By.xpath("//input[@id='check-box-four']"));
+		boolean isCheckBoxFourSelected = checkBoxFour.isSelected();
+		
+		assertTrue(isCheckBoxOneSelected,"Check box one is unselected");
+		assertTrue(isCheckBoxTwoSelected,"Check box two is unselected");
+		assertTrue(isCheckBoxThreeSelected,"Check box three is unselected");
+		assertTrue(isCheckBoxFourSelected,"Check box four is unselected");
+		assertTrue(isSelectAllButtonDisplayed, "Select all button is not displayed");
+	}
+	
+	@Test
+	public void checkMultipleCheckBoxUnSelected()
+	{
+		driver.navigate().to("https://selenium.obsqurazone.com/check-box-demo.php");
+		WebElement selectAllButton = driver.findElement(By.xpath("//input[@id='button-two']"));
+		selectAllButton.click();
+		WebElement  unSelectAll = driver.findElement(By.xpath("//input[@value='Unselect All']"));
+		unSelectAll.click();
+		boolean isSelectAllButtonDisplayed = unSelectAll.isDisplayed();
+		
+		WebElement checkBoxOne = driver.findElement(By.xpath("//input[@id='check-box-one']"));
+		boolean isCheckBoxOneSelected = checkBoxOne.isSelected();
+		WebElement checkBoxTwo = driver.findElement(By.xpath("//input[@id='check-box-two']"));
+		boolean isCheckBoxTwoSelected = checkBoxTwo.isSelected();
+		WebElement checkBoxThree = driver.findElement(By.xpath("//input[@id='check-box-three']"));
+		boolean isCheckBoxThreeSelected = checkBoxThree.isSelected();
+		WebElement checkBoxFour = driver.findElement(By.xpath("//input[@id='check-box-four']"));
+		boolean isCheckBoxFourSelected = checkBoxFour.isSelected();
+		
+		assertTrue(isSelectAllButtonDisplayed, "Unselect all is displayed");
+		assertFalse(isCheckBoxOneSelected, "Check box one is selected");
+		assertFalse(isCheckBoxTwoSelected, "Check box two is selected");
+		assertFalse(isCheckBoxThreeSelected, "Check box three is selected");
+		assertFalse(isCheckBoxFourSelected, "Check box four is selected");	
 	}
 
 }
--- src/test/java/com/obsqura/TestNGFramework/RadioButtonDemo.java
+++ src/test/java/com/obsqura/TestNGFramework/RadioButtonDemo.java
@@ -1,5 +1,8 @@
 package com.obsqura.TestNGFramework;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -15,5 +18,35 @@ public class RadioButtonDemo extends Base {
 		Boolean isRadioButtonSelected = radioButtonElement.isSelected();
 
 	}
+	
+	@Test
+	public void checkGroupRadioButtons()
+	{
+		String expectedResult = "Gender : Female"+"\n"+"Age group: 19 to 44";
+		
+		driver.navigate().to("https://selenium.obsqurazone.com/radio-button-demo.php");
+		WebElement patientsGender = driver.findElement(By.xpath("//input[@id='inlineRadio21']"));
+		patientsGender.click();
+		boolean isSelectedPatientsGender = patientsGender.isSelected();
+		
+		WebElement patientsAgeGroup = driver.findElement(By.xpath("//input[@id='inlineRadio23']"));
+		patientsAgeGroup.click();
+		boolean isSelectedPatientsAgeGroup = patientsAgeGroup.isSelected();
+		
+		WebElement getResultButton = driver.findElement(By.xpath("//button[@id='button-two']"));
+		getResultButton.click();
+		boolean getResultButtonIsDisplayed = getResultButton.isDisplayed();
+		
+		WebElement getResultText = driver.findElement(By.xpath("//div[@id='message-two']"));
+		String textDataOfGetResult = getResultText.getText();
+		
+		assertTrue(isSelectedPatientsGender, "Patients gender not selected");
+		assertTrue(isSelectedPatientsAgeGroup, "Patients age group is not selected");
+		assertTrue(getResultButtonIsDisplayed, "Get result button is not displayed");
+		assertEquals(textDataOfGetResult, expectedResult, "Expected and actual result are not same");
+		
+		
+	}
+
 
 }
